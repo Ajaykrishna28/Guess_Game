4c39362eeb6396c958cc05ed44bf19115600eab4
Ajay Krishna <145196355+Ajaykrishna28@users.noreply.github.com>
2024-08-17 21:56:37 +0530

    Add files via upload

---
+++ Guess_game.py
@@ -0,0 +1,36 @@
+# -*- coding: utf-8 -*-
+"""
+This code is about guessing 
+Created on Fri Aug  9 14:09:11 2024
+
+@author: ajayk
+"""
+
+import random
+
+ # Generate a random number between 1 and 100
+random_number = random.randint(1, 100)
+attempts = 0
+
+print(" Welcome to the Number Guessing Game! ")
+print(" I have generated a random number between 1 and 100. ")
+print(" Can you guess what it is? ")
+
+while True:
+    try:
+        guess = int(input("\n Enter your guess: "))
+        attempts += 1
+
+        if random_number - guess > 5:
+            print(" You are too below! Try again.")
+        elif guess < random_number:
+            print(" You are just below the number!! Try once again. ")
+        elif guess - random_number > 5:
+            print(" You are too above! Try again ")
+        elif guess > random_number:
+            print(" You are just above the number!! Try once again. ")
+        else:
+            print(f" Congratulations! You've guessed the number correctly in {attempts} attempts. ")
+            break
+    except ValueError:
+        print(" Invalid input. Please enter a number between 1 and 100. ")
